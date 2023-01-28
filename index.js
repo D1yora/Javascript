26b490a55ca3b048e5411218bc7bc223bd3b1f4a
D1yora <dioora2o3@gmail.com>
2023-01-28 12:12:23 +0500

    homework & notes

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log(undefined == undefined);
+
+// git init
+// git remote add origin Url
+
+// git add .
+// git commit -m ""
+// git push origin master
+
+// git submodule add <url> lesson 1
+// git rm --cached lesson 1
+// git rm -fr .git // how to del git file from localhost
