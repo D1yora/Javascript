386f32331a475b7c88ed7667127c54a80dbfcc47
Diyora Soyibjonova <87473783+D1yora@users.noreply.github.com>
2023-02-02 23:47:27 +0500

    Add files via upload

---
+++ lesson 5/homework.js
@@ -0,0 +1,61 @@
+// homework
+
+// 1-task
+
+// let browser = 'Operaaa';
+
+// switch (browser) {
+//   case 'Edge' :
+//     alert('You have got the edge!');
+//     break;
+//   case 'Chrome' :
+//     alert('You have got the Chrome!');
+//     break;
+//   case 'Firefox' :
+//     alert('You have got the Friefox!');
+//     break;
+//   case 'Safari' :
+//     alert('You have got the Friefox!');
+//     break;
+//   case 'Opera' :
+//     alert('Okay, we support this browsers too!');
+//     break;
+//   default:
+//     alert('We hope this page looks ok!')
+// }
+
+// 2-task
+
+// let a = +prompt('Raqamni kiriting', 'write smth in here')
+
+// if (a == 5) {
+//   console.log(5);
+// }
+// if (a == 10) {
+//   console.log(10);
+// }
+// if (a == 20 || a == 30) {
+//   console.log(20,30);
+// }
+
+// 3-task
+
+// let i = 3;
+
+// while (i) {
+//   console.log(i--);
+// }
+
+// bu yerda while loop ishlatilib, i-- bolgani uchun teskari 3 2 1ga sanadi.
+
+// 4-task
+
+// let i = 0;
+// while (i++ < 5) console.log(i);
+
+// 5gacha sanab consoleda chiqar deb buyruq berdik va u bizga 0dan 5gach chqarb berdi.
+
+let i = 0;
+while (++i < 5) console.log( i );
+
+// bu yerda consoleda brdan 4gacha chiqdi, lekin why?*  !!!
