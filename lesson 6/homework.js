d6f7330c7ac4df230f9f4b38bf42e131a1fa9e74
Diyora Soyibjonova <87473783+D1yora@users.noreply.github.com>
2023-02-05 23:42:44 +0500

    functions

---
+++ lesson 6/homework.js
@@ -0,0 +1,48 @@
+// HOMEWORK
+
+// task 1
+// function
+
+
+// for (let i = 0; i < 3; i++) {
+//   console.log( `number ${i}!`);
+// }
+
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   } else {
+//     return confirm('did your parents allow you?')
+//   }
+// }
+
+// okay, bu yerda function degan funksiya ichida if statement yozilgan.
+
+// function checkAge(age) {
+//   return (age > 18) ? true : confirm('did your parents allow you?');
+// }
+
+// task 2
+
+// min(2 , 5) == 2
+// min(3 , -1) == -1
+// min(1 , 1) == 1
+
+// function min(a, b) {
+//   if(a < b) {
+//     return a
+//   } else b;
+// }
+
+// min()
+
+// task 3
+
+// function pow(a, b) {
+//   let num = 1;
+//   for (let i = 0; i < b; i++) {
+//     num = num * a
+//   } console.log(num);
+// }
+
+// pow(5, 5)
