7346f61ff665967e1eeea25b2e8191c1d09c9489
Diyora Soyibjonova <87473783+D1yora@users.noreply.github.com>
2023-02-10 08:13:26 +0500

    object basic

--- lesson 7/homework.js
+++ lesson 7/homework.js
@@ -1,1 +1,43 @@
+// objects
 
+// primitive - value,
+// non-primitive - manzil, object, function, array
+
+// let account = 'webbrain';
+
+// console.log(account);
+
+// {tlt:}
+
+let account = {
+  name: "webbrain academy",
+  major: "frontend",
+  founded: 2020,
+  "full name": "webbrain IT academy",
+};
+
+key: "major",
+
+console.log(account.key);
+console.log(account[key]);
+console.log(account["key"]);
+
+// console.log(account["full name"]);
+
+// const object = {};
+// object.name = ["john"];
+// object.surname = ["smith"];
+// object.name = "pete";
+// delete object.name;
+
+// console.log(object);
+
+// task 2
+
+// / const salaries = {
+  //     john: 100,
+  //     ann: 160,
+  //     pete: 130,
+  // }
+
+  // let num = 0;
