0fd5853e164a37242b801320cd119fc70a249772
Diyora Soyibjonova <87473783+D1yora@users.noreply.github.com>
2023-02-20 15:56:48 +0500

    notes(nums&string)

---
+++ lesson 9/notes.js
@@ -0,0 +1,8 @@
+// numbers & string
+
+// console.log(1e-2); // kasr songa otb ketadi.
+// console.log(1e7); // e orqali yoniga raqam yozib shuncha nol qoshishimiz mumkin.
+// console.log(1e500); //meyordan otb ketsa infinity chqadi.
+
+console.log(0.1 + 0.01);
+console.log();
