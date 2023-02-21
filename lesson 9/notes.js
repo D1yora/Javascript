7d2359a4e944cd4ec78947494c1111b1f947684c
Diyora Soyibjonova <87473783+D1yora@users.noreply.github.com>
2023-02-21 23:31:06 +0500

    notess

--- lesson 9/notes.js
+++ lesson 9/notes.js
@@ -6,3 +6,26 @@
 
 console.log(0.1 + 0.01);
 console.log();
+
+// toFixed(1) - qiymarlanri kasrdan keyin nechta son qolishi. tofixed avotimatik tarzda numberni stringga aylantrb yuboradi.
+// Numberga convert qilib yuborhs uchun +, NUmber dan foydalaniladi.
+// let num = 0.1 + 0.2
+
+// console.log(Number(num.toFixed(1)));
+
+
+// stringni numberga o'zgartsh uchun NUmber yoki pareint,parsefloat dan foydalansak boladi.
+// let num = "0.1 + 0.2"
+// console.log(parseInt(num)); //ko'rgan birinchi raqamini oladi.
+// console.log(parseFloat(num)); // ko'rgan birnchi fraction'ni oladi.
+// console.log(Number(num)); // NaN qaytadi chunki raqam bollmagan narsalar ustida arifmetik amallar bajarilyabti.
+// console.log(Number(eval(num))); // string'da amallarni bajarish convert uchun evaldan foydalansak boladi
+// console.log(Number.parseInt(num));
+// console.log(Number.parseFloat(num));//number bilan ham bersak boladi.
+
+let num = 12;
+console.log(num.toString(6)); //raqamni stringga o'girish
+
+// .tofixed() - kasrdan keyin nechta son qolishi
+// .toString() (base)- raqamni stringga o'girish, descimal to binary
+// .toFixed (count) - nechta kasr qismi qolishi
