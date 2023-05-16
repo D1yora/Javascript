63b91db70a6355d4fc2fb955fdbcaf4b85a41842
Diyora Soyibjonova <87473783+D1yora@users.noreply.github.com>
2023-05-16 23:27:28 +0500

    Datastructure | JSON | New data()

---
+++ lesson 13/index.js
@@ -0,0 +1,9 @@
+let obj = {name: "webb", title: "IT center", data: {year: 1457}};
+
+let name = "web";
+let {name: Newname, title, age, data: {year}} = obj;
+console.log(obj.name, year);
+
+let ar = ["orange", "orange", "Kiwi"];
+
+let [] = ar;
